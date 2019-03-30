1ff30d8915cbb669182ecc52468a1f223d8edd8a
Guilherme Humberto Jansen <contact.ghjansen@gmail.com>
2019-03-29 22:07:58 -0300

    Reverting changes related to the attempt of using JPA without database.

--- src/main/java/com/ghjansen/checkout/CheckoutConfiguration.java
+++ src/main/java/com/ghjansen/checkout/CheckoutConfiguration.java
@@ -3,15 +3,10 @@ package com.ghjansen.checkout;
 import com.ghjansen.checkout.persistence.repository.CartItemRepository;
 import com.ghjansen.checkout.persistence.repository.CartRepository;
 import com.ghjansen.checkout.persistence.repository.ProductRepository;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class CheckoutConfiguration {
 
     @Bean
