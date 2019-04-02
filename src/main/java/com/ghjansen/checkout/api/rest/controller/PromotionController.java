39c4c7f6f824d5eda2369a086688aef1cb0ae2e4
Guilherme Humberto Jansen <contact.ghjansen@gmail.com>
2019-04-02 00:41:36 -0300

    Inclusion of missing mapping for Promotion controller.

--- src/main/java/com/ghjansen/checkout/api/rest/controller/PromotionController.java
+++ src/main/java/com/ghjansen/checkout/api/rest/controller/PromotionController.java
@@ -3,6 +3,7 @@ package com.ghjansen.checkout.api.rest.controller;
 import com.ghjansen.checkout.persistence.model.Promotion;
 import com.ghjansen.checkout.service.PromotionService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +23,13 @@ public class PromotionController {
     public @NotNull Iterable<Promotion> getPromotions(){
         synchronized (this.promotionService){
             return this.promotionService.getAllPromotions();
+        }
+    }
+
+    @GetMapping(value = {"/{id}"})
+    public @NotNull Promotion getPromotion(@PathVariable("id") Long id){
+        synchronized (this.promotionService){
+            return this.promotionService.getPromotion(id);
         }
     }
 }
